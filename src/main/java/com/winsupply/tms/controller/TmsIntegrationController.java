921e36a8b1498ec79df5559cfd19d16085716fdd
manojVeldandi <mkveldandie@winsupply.in>
2022-11-23 17:44:56 +0530

    Basic Http client call for GraphQL

--- src/main/java/com/winsupply/tms/controller/TmsIntegrationController.java
+++ src/main/java/com/winsupply/tms/controller/TmsIntegrationController.java
@@ -1,14 +1,17 @@
 package com.winsupply.tms.controller;
 
+import com.winsupply.tms.utility.GraphQLClient;
+import org.springframework.graphql.client.ClientGraphQlResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class TmsIntegrationController {
 
     @GetMapping("/tmstest")
-    public void test(){
+    public ClientGraphQlResponse test(){
         System.out.println(" This is tms test controller ");
-
+        return GraphQLClient.clientCall();
     }
 }
